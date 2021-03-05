407106cba41c1ae42481b410ac2a20775e277dd2
Hannecart Loïc <76540590+HanLoi@users.noreply.github.com>
2021-03-05 14:31:35 +0100

    fin js + test % css

--- script.js
+++ script.js
@@ -14,13 +14,11 @@
     ];
 
     //random word
-
     function randomWord () {
         return Math.floor(Math.random()*10)
     }
 
     //changement des tirets les lettres du secretWord
-
     function changeTiret(letter) {
     
         let indices = [];
@@ -34,50 +32,43 @@
 
         indices.forEach(indice => {
             spanTable[secretWordSplit.indexOf(letter, indice)].innerHTML = letter;
-    
         });
     }
     // function état du jeu
     function stateGame(a) {
 
         let bool = true;
-        
         spanTable.forEach(element => {
             if (element.innerHTML == "_"){
                 bool = false
             }
         });
-        console.log(bool)
         if (bool){
             document.getElementById("endgame").innerHTML = " ACCES AUTHORIZE !"
         }
         if ( a == 0){
             document.getElementById("endgame").innerHTML=" ACCES DENIED !"
             bout.style.display = "none";
-
-    }}
-
+        }
+    }
+    //function récupérer un mot aléatoire
     function restartWord() {
-    
+
         randomNum= randomWord()
         secretWord = listWord[randomNum]["word"];
         secretWordSplit = secretWord.split("");
         display.innerHTML = "";     
         span()
-        }
+    }
+    //function crée span underscore    
+    function span() { 
 
-
-    function span() {        
         for (let i=0; i <secretWordSplit.length; i++) {
-            var score = document.createElement('span');
+            let score = document.createElement('span');
             score.innerHTML="_";
-            score.style.fontSize='50px'
-                    
+            score.style.fontSize='50px'                  
             display.appendChild(score);}
     }
-
-    
-
     //variable
     let randomNum= randomWord();
     let secretWord = listWord[randomNum]["word"];
@@ -87,53 +78,50 @@
     let display = document.getElementById('displayWord');
     let bout = document.querySelector(".alphabet");   
     let boutons = bout.querySelectorAll("button");
-    
-
     //affichage
     document.getElementById("displayLetter").innerHTML = "You have already tape : "+ displayLetter;
     document.getElementById("count").innerHTML="ATTEMPT(S) LEFT :  "+ live;
-
     //création des première spans
     span()
-
     //acquérir l'input de lettre
     let table = document.getElementById("displayWord");
     let spanTable = table.querySelectorAll("span");
 
-        boutons.forEach(btn =>
-            btn.addEventListener(
-                "click", () => {
+    boutons.forEach(btn =>
+        btn.addEventListener(
+            "click", () => {
 
-                    let b = btn.innerHTML;
-                    let h = b.toLowerCase();
+                let b = btn.innerHTML;
+                let h = b.toLowerCase();
+                if (secretWordSplit.includes(h) == true){
+                    changeTiret(h)
+                }else {
+                    live -= 1
+                    displayLetter.push(h)
+                    changeTiret(h)
+                }
 
-                    if (secretWordSplit.includes(h) == true){
-                        changeTiret(h)
-                    }else {
-                        live -= 1
-                        displayLetter.push(h)
-                    }
-
-                    document.getElementById("count").innerHTML="ATTEMPT(S) LEFT :  "+ live;
-                    document.getElementById("displayLetter").innerHTML = "You have already tape : "+ displayLetter;
-                    stateGame(live);
-                }));
-
+                document.getElementById("count").innerHTML="ATTEMPT(S) LEFT :  "+ live;
+                document.getElementById("displayLetter").innerHTML = "You have already tape : "+ displayLetter;
+                stateGame(live);
+         })
+    );
+    //évènement affichage indice
     document.getElementById("clue").addEventListener("click", () => {
         document.getElementById("displayClue").innerHTML=" : " + listWord[randomNum]["clue"]
-     })
+    })
 
+    //évènement boutton restart
     document.getElementById("restart").addEventListener("click", () => {
         restartWord();
         live = 7;
-        stateGame(live)
         document.getElementById("count").innerHTML="ATTEMPT(S) LEFT :  "+ live;
         bout.style.display = "";
         document.getElementById("endgame").innerHTML = "ENTER THE PASSWORD !";
         document.getElementById("displayClue").innerHTML=" :  ***********************************";
         displayLetter=[];
         document.getElementById("displayLetter").innerHTML = "You have already tape : "+ displayLetter;
-     })
+    })
 
 
 
